8b9935331fc68e211ba4ee83100c014cc2d9753e
SalehAskari <salehaskari.4992@gmail.com>
2026-02-11 10:31:33 +0330

    update.

--- referee-teakwondo/resources/views/index/master.blade.php
+++ referee-teakwondo/resources/views/index/master.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Soccer &mdash; Website by Colorlib</title>
+    <title>Teakwondo &mdash; Referee Teakwondo</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
